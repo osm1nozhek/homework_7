38d360d1c42fb72a1cae40bdbb5ca6df84eb133e
osm1nozhek <snurenkoviktoria@gmail.com>
2023-05-04 22:26:27 +0300

    .

--- polls/models.py
+++ polls/models.py
@@ -6,10 +6,10 @@ class Teacher(models.Model):
     name = models.CharField(max_length=200)
     subjects = models.CharField(max_length=200)
 
-# class Student(models.Model):
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
 
 class Group(models.Model):
     name = models.CharField(max_length=100)
