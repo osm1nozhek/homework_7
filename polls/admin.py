87e0c835525cd34192a824589f4558c7c08cb405
osm1nozhek <snurenkoviktoria@gmail.com>
2023-05-05 02:28:43 +0300

    .

--- polls/admin.py
+++ polls/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 
 from polls.models import Teacher, Group
 
+
 # Register your models here.
 # admin.site.register(Teacher)
 # admin.site.register(Group)
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     ordering = ["name"]
+
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 
-
-
 class Teacher(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    subject=models.CharField(max_length=200)
+    subject = models.CharField(max_length=200)
 
     def __str__(self):
         return f"{self.name} "
+
+
 class Student(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
@@ -17,10 +17,12 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
 class Group(models.Model):
     name = models.CharField(max_length=100)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     student = models.ManyToManyField(Student)
+
     def __str__(self):
         return f"{self.name}"
-
